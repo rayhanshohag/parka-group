beef12761dc95926d32a3a8e8b6034e62a8dc9e4
rayhanshohag <rayhan.sh@gmail.com>
2017-03-05 13:39:28 -0500

    Create sanitize_comments.php

---
+++ sanitize_comments.php
@@ -0,0 +1,16 @@
+<?php
+function clean_data( $input )
+{
+    $input = trim( htmlentities( strip_tags( $input,"<a><b><i><img>" ) ) );
+
+    if( get_magic_quotes_gpc() )
+        $input = stripslashes( $input );
+
+    $input = mysql_real_escape_string( $input );
+    return $input;
+}
+
+
+$filtered_comment = clean_data( $_POST['comment'] );
+
+?>
