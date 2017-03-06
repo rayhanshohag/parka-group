0ba509a68b3afd8ef6a8699cc58fbeeea3dc87d8
rayhanshohag <rayhan.sh@gmail.com>
2017-03-06 14:54:14 -0500

    Update sanitize_comments.php
    1. Write a function that will sanitize input.
    Consider an application in which a website user can post comments to an article. Write a function that will sanitize the comment submission. The function:
    -        should allow 4 basic HTML tags are allowed: a, b, i, img
    -        should analyze security issues and sanitize input accordingly
    -        may consist of calls to empty functions that describe what you want to do (i.e. you do not have to write code for these called functions)

--- sanitize_comments.php
+++ sanitize_comments.php
@@ -1,3 +1,10 @@
+<!--1. Write a function that will sanitize input.
+Consider an application in which a website user can post comments to an article. Write a function that will sanitize the comment submission. The function:
+-        should allow 4 basic HTML tags are allowed: a, b, i, img
+-        should analyze security issues and sanitize input accordingly
+-        may consist of calls to empty functions that describe what you want to do (i.e. you do not have to write code for these called functions)
+-->
+
 <?php
 function clean_data( $input )
 {
@@ -15,4 +22,4 @@ $filtered_comment = clean_data( $_POST['comment'] );
 
 ?>
 
-<!----->
+<!------->
